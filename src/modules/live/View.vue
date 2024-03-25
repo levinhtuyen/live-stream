268fbbfae41845fdab521e5670d826512496943f
Lê Vĩnh Tuyến <42595840+levinhtuyen@users.noreply.github.com>
2024-03-25 23:17:41 +0700

    ediut page

--- src/modules/live/View.vue
+++ src/modules/live/View.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1>Live stream</h1>
-    <video id="dplayer"></video >
+    <video autoplay="true"
+    muted="muted" id="dplayer"></video >
   </div>
 </template>
 
 <script>
   import flvjs from 'flv.js'
-  //import DPlayer from 'dplayer';
+//import DPlayer from 'dplayer';
 
 export default {
   name: 'Typography',
@@ -19,52 +19,34 @@ export default {
 
     }
   },
-  mounted() {
+  mounted: function () {
+    this.$nextTick(function () {
     if (flvjs.isSupported()) {
         var videoElement = document.getElementById('dplayer');
-        if(videoElement) {
-          var flvPlayer = flvjs.createPlayer({
-            type: 'flv',
-            url: 'https://pull.niur.live/live/stream-9912060_lhd.flv?txSecret=d270064b33342945cfdfaa1243c3b19f&txTime=6601d390'
-          });
-          flvPlayer.attachMediaElement(videoElement);
-          flvPlayer.load();
-          
-        }
-    }
-    let vii = flvPlayer.play();
-    if (vii!== undefined) {
-      vii.then(() => {
-        flvPlayer.play();
-      }).catch(error => {
-        console.log('error :>> ', error);
-      })
-    }
-  },
-  computed(){
-
-  },
-  watch() {
-    if (flvjs.isSupported()) {
-      var videoElement = document.getElementById('dplayer');
-      if(videoElement) {
         var flvPlayer = flvjs.createPlayer({
-          type: 'flv',
-          url: 'https://pull.niur.live/live/stream-9912060_lhd.flv?txSecret=d270064b33342945cfdfaa1243c3b19f&txTime=6601d390'
+            type: 'flv',
+            url: 'https://pull.niur.live/live/stream-9912035_lhd.flv?txSecret=d50d022f56a54817ee16181b76653075&txTime=6601f604'
         });
         flvPlayer.attachMediaElement(videoElement);
-      }
+        flvPlayer.load();
+        flvPlayer.play();
     }
-    let vii = flvPlayer.play();
-    if (vii!== undefined) {
-    vii.then(() => {
-      flvPlayer.play();
-    }).catch(error => {
-      console.log('error :>> ', error);
-    });
-}
+  })
   },
-  
+  updated: function () {
+    this.$nextTick(function () {
+    if (flvjs.isSupported()) {
+        var videoElement = document.getElementById('dplayer');
+        var flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            url: 'https://pull.niur.live/live/stream-9912035_lhd.flv?txSecret=d50d022f56a54817ee16181b76653075&txTime=6601f604'
+        });
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.load();
+        flvPlayer.play();
+    }
+  })
+   },
 }
 </script>
 
